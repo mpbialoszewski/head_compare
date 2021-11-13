3f7cd66f0aef7d701b89855a37b51394746bb278
mpbialoszewski <m.p.bialoszewski@outlook.com>
2021-11-13 13:33:57 +0000

    Refactoring - not needed variables

--- header_compare.rb
+++ header_compare.rb
@@ -1,28 +1,27 @@
 require 'CSV'
 
 # TODO- enable running the program by passing arguments in CMD ('ruby header_compare.rb benchmark_file #{location_of_file}')
+
 # TODO - convert whole program to OOP 
-# TODO - refactoring the names of variables 
 
 #Benchmark file reading 
         benchmark_file = CSV.read('benchmark.csv',headers: true).headers.to_s
-        benchmark_file_split = (benchmark_file.tr('"', '').gsub!(', ', " ")).split
-        benchmark_array = Array.new
-        benchmark_array.push(benchmark_file_split)
+        benchmark_array = benchmark_file.gsub!(',',' ').split(' ')
+        puts benchmark_array.length
+
 
 #Compared file reading 
         compared_file = CSV.read("file1.csv", headers: true).headers.to_s
-        compared_file_split = (compared_file.tr('"', '').gsub!(', ', " ")).split
-        parts_array = Array.new
-        parts_array.push(compared_file_split)
+        compared_array = compared_file.gsub!(',',' ').split(' ')
+        puts compared_array.length
 
-    if (benchmark_array - parts_array).empty?
+    if (benchmark_array - compared_array).empty?
         print "All good, the headers are the same"
     else 
     # TODO - point which elements of array are missing 
         warn "Oh no, there are some headers missing"
-        warn"Expected number of headers: #{benchmark_file_split.length}, compared headers number: #{compared_file_split.length} "
-        warn "Missing elements of compared header #{benchmark_file_split - compared_file_split}"
+        warn"Expected number of headers: #{benchmark_array.length}, compared headers number: #{compared_array.length} "
+        warn "Missing elements of compared header #{benchmark_array - compared_array}"
     end
 # TODO - send sample email with error message to me
 
