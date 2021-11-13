95b9417a64bfa2e4ab2eee0d62593f6f6e424843
mpbialoszewski <m.p.bialoszewski@outlook.com>
2021-11-13 13:49:49 +0000

    Removing spaces in arrays

--- header_compare.rb
+++ header_compare.rb
@@ -1,4 +1,5 @@
 require 'CSV'
+require 'mail'
 
 # TODO- enable running the program by passing arguments in CMD ('ruby header_compare.rb benchmark_file #{location_of_file}')
 
@@ -6,13 +7,12 @@ require 'CSV'
 
 #Benchmark file reading 
         benchmark_file = CSV.read('benchmark.csv',headers: true).headers.to_s
-        benchmark_array = benchmark_file.gsub!(',',' ').split(' ')
+        benchmark_array = (benchmark_file.tr('"', '').gsub!(', ', "\n")).split
         puts benchmark_array.length
-
 
 #Compared file reading 
         compared_file = CSV.read("file1.csv", headers: true).headers.to_s
-        compared_array = compared_file.gsub!(',',' ').split(' ')
+        compared_array = (compared_file.tr('"', '').gsub!(', ', "\n")).split
         puts compared_array.length
 
     if (benchmark_array - compared_array).empty?
@@ -21,17 +21,11 @@ require 'CSV'
     # TODO - point which elements of array are missing 
         warn "Oh no, there are some headers missing"
         warn"Expected number of headers: #{benchmark_array.length}, compared headers number: #{compared_array.length} "
-        warn "Missing elements of compared header #{benchmark_array - compared_array}"
+        warn "Missing elements of compared header: #{benchmark_array - compared_array}"
     end
 # TODO - send sample email with error message to me
 
 
-
-# raise ArgumentError, "Oh no, there are some headers missing"
-# rescue => e
-#   puts "Expected number of headers: #{e.benchmark_file_split.length}, compared headers number: #{e.compared_file_split.length} "
-#   warn "Missing elements of compared header #{benchmark_file_split - compared_file_split}"
-# end
 
 
 
